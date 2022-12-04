fe1e3eecaa2082b7e4b412e21e2940b48fb64503
micadevcon <micadevcon@mail.ru>
2022-12-04 22:11:11 +0400

    final

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace WhoLikesIt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] name = new string[] {"1asdas","2dsa","3sds" };
+            Console.WriteLine(Likes(name));
+        }
+        public static string Likes(string[] name)
+        {
+            switch (name.Length)
+            { 
+                case 0:
+                    return "no one likes this";
+                case 1:
+                    return name[0]+ " likes this";
+                case 2:
+                    return name[0]+ " and "+ name[1] +" like this";
+                case 3:
+                    return name[0]+", "+ name[1]+" and "+ name[2]+ " like this";
+                default:
+                    return name[0]+ ", "+ name[1]+" and "+ (name.Length-2) + " others like this";
+            }
+            throw new NotImplementedException();
         }
     }
 }
